8778431266103b6f0ab4a957dd3961b9bf725f13
Akanksha-neural-networks <akanksha.prasad25@gmail.com>
2024-03-28 17:50:15 +0530

    config

---
+++ src/config/configuration.ts
@@ -0,0 +1,24 @@
+export default () => ({
+  snowflake: {
+    account: '',
+    username: '',
+    password: '',
+    region: '',
+    database: '',
+    authenticator: '',
+  },
+  mysql: {
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'omsairam',
+    database: 'College',
+  },
+  postgres: {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'college',
+    password: 'omsairam',
+    port: 5432,
+  },
+});
