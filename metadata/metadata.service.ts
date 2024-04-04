6b74fd79a84ed9345445f8135f7984d77659eaef
Akanksha Prasad <101962662+Akanksha-neural-networks@users.noreply.github.com>
2024-04-04 11:04:19 +0530

    Add files via upload
    Updated Mywork

---
+++ metadata/metadata.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  OnApplicationShutdown,
+  NotFoundException,
+} from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import { MysqlService } from 'src/mysql/mysql.service';
+import { PostgresService } from 'src/postgres/postgres.service';
+import { SnowflakeService } from 'src/snowflake/snowflake.service';
+
+@Injectable()
+export class MetadataService {
+  private queries: { [key: string]: string };
+
+  constructor(
+    private readonly snowflakeService: SnowflakeService,
+    private readonly mysqlService: MysqlService,
+    private readonly postgresService: PostgresService,
+  ) {
+    this.queries = this.readQueriesFromFile();
+  }
+
+  private readQueriesFromFile(): { [key: string]: string } {
+    const filePath = path.resolve(
+      process.cwd(),
+      'src',
+      'metadata',
+      'resources',
+      'metadata-queries.json',
+    );
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(fileContent);
+  }
+
+  async getMetadata(@Query('source') source: string): Promise<any> {
+    try {
+      if (!source || !this.queries[source]) {
+        throw new NotFoundException('Invalid or missing source');
+      }
+      const query = this.queries[source];
+      console.log({ query });
+      let result;
+      switch (source) {
+        case 'snowflake':
+          result = await this.snowflakeService.executeQuery(query);
+          return result[0].GET_DATABASES_SCHEMAS_TABLES;
+        case 'mysql':
+          result = await this.mysqlService.executeQuery(query);
+          return result[0].result;
+        case 'postgres':
+          result = await this.postgresService.executeQuery(query);
+          return result[0].get_metadatanew;
+        default:
+          throw new NotFoundException('Invalid source');
+      }
+    } catch (error) {
+      console.error('Error executing query:', error);
+      throw new NotFoundException('Failed to execute query');
+    }
+  }
+}
