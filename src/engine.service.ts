3036269e40ab754d730e8a219e3dd504dc2d1b07
Aashika Sharma <119496187+Aashika-Sharma@users.noreply.github.com>
2024-04-17 14:54:21 +0530

    Update and rename app.service.ts to engine.service.ts

---
+++ src/engine.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+
+@Injectable()
+export class EngineService {
+  private readonly engineData: { name: string }[];
+
+  constructor() {
+    try {
+      const fileContent = fs.readFileSync('./src/engine.json', 'utf8');
+      const jsonData = JSON.parse(fileContent);
+      this.engineData = jsonData.engines || [];
+    } catch (error) {
+      console.error('Error reading or parsing engine.json:', error);
+      this.engineData = [];
+    }
+  }
+
+  getAllEngineNames() {
+    return this.engineData.map(engine => engine.name);
+  }
+}
